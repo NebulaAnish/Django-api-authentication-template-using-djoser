9904e6563a1326f319629419ade842775519bf63
Nebula <nebulaanish@gmail.com>
2024-04-14 18:30:46 +0545

    fix : env fetch

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -50,11 +50,10 @@ SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
-
 #  Email backend setups
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_USER = os.getenv("EMAIL_USERNAME")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
