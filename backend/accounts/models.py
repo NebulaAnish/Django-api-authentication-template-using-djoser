9e3384f2bdfa637d609ac5821842e13c4d1c247e
Nebula <nebulaanish@gmail.com>
2024-04-14 12:34:35 +0545

    Feat : Modified CustomUser to use AbstractUser before migration

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,19 +1,17 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
 
-class CustomUser(AbstractBaseUser):
+
+class CustomUser(AbstractUser):
+    username = None
     email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    data_joined = models.DateTimeField(default=timezone.now)
-    
-    USERNAME_FIELD = 'email'
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-    
+
     objects = CustomUserManager()
 
     def __str__(self) -> str:
